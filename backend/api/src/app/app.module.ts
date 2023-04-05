39e7eaa83a4a4384c2fabb0acb3fa21432a63213
Leontev <leontevdima123@gmail.com>
2023-04-05 14:00:02 +0800

    Add graphql schema and config

--- backend/api/src/app/app.module.ts
+++ backend/api/src/app/app.module.ts
@@ -3,14 +3,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { environment } from '../enviroments/environment';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { AppResolver } from './app.resolver';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       ...environment.connection,
     }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      typePaths: ['./**.graphql'],
+      context: ({ req }: { req: never }) => ({ req }),
+      driver: ApolloDriver,
+      playground: true,
+    }),
   ],
   controllers: [AppController],
-  providers: [],
+  providers: [AppResolver],
 })
 export class AppModule {}
